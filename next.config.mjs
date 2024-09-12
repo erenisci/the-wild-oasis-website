2303a93ee7125615616425a3148687ac26406cd8
Eren İşci <isci.eren@gmail.com>
2024-09-12 14:16:06 +0300

    .config

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.SUPABASE_HOSTNAME,
+        port: '',
+        pathname: process.env.SUPABASE_PATHNAME,
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // output: 'export',
+};
+
+export default nextConfig;
